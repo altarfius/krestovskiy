4f1fea6b1558087dc91e8534e5f7f3fa13b48ca1
Дмитрий Натовский <altarfius@gmail.com>
2019-02-24 08:30:10 +0300

    Добавил данные тестовой бд

--- config/db.php
+++ config/db.php
@@ -7,6 +7,12 @@ return [
     'password' => '',
     'charset' => 'utf8',
 
+//    'class' => 'yii\db\Connection',
+//    'dsn' => 'mysql:host=localhost;dbname=altarfius2',
+//    'username' => 'altarfius2',
+//    'password' => 'K.c.ifvjz1',
+//    'charset' => 'utf8',
+
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
