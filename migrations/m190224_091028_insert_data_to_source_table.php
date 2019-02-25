ec6e94e0131afcdefec7e856cb159684c905abdb
Дмитрий Натовский <altarfius@gmail.com>
2019-02-25 08:21:45 +0300

    Источники (убрать дубляжи)

---
+++ migrations/m190224_091028_insert_data_to_source_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190224_091028_insert_data_to_source_table
+ */
+class m190224_091028_insert_data_to_source_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->batchInsert('source', [
+            'name'
+        ], [
+            ['Hh'],
+            ['Superjob'],
+            ['Worki'],
+            ['Avito'],
+            ['Restojob'],
+            ['От знакомых'],
+            ['Vk.com'],
+            ['Instagram'],
+            ['Наш сайт'],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        return true;
+    }
+
+}
